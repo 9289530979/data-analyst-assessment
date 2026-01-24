858a329d3aaab4d24a89646ad8f6316fe4a06a15
Amit Kumar <Amit.kumar.workmail@gmail.com>
2026-01-24 16:18:37 +0530

    Update 04_funnel_analysis.sql

--- 04_funnel_analysis.sql
+++ 04_funnel_analysis.sql
@@ -1,37 +1,23 @@
+1. Signup/ 1000
 SELECT COUNT(DISTINCT customer_id) AS signup_users
 FROM customers
-Not sure 
 
 
-1.Trial/ 676 COUNT
+2. Trial/ 676 COUNT
 SELECT COUNT(DISTINCT customer_id)
 FROM events
 WHERE event_type = 'trial_start'
 
-2. Activated/ 404 COUNT 
+3. Activated/ 404 COUNT 
 SELECT COUNT(DISTINCT customer_id)
 FROM events
 WHERE event_type = 'activated'
 
-3. Paid/ 906 COUNT
+4. Paid/ 906 COUNT
 SELECT COUNT(DISTINCT customer_id)
 FROM subscriptions
 
-4. Churned/ 223 COUNT 
+5. Churned/ 223 COUNT 
 SELECT COUNT(DISTINCT customer_id)
 FROM subscriptions
 WHERE status = 'canceled'
-
-
-
-
-Conversion Rate: Signup → Trial
-SELECT 
-    s.signup_users,
-    t.trial_users,
-    ROUND(t.trial_users / s.signup_users * 100, 2) AS signup_to_trial_pct
-FROM
-    (SELECT COUNT(DISTINCT customer_id) AS signup_users FROM customers) s,
-    (SELECT COUNT(DISTINCT customer_id) AS trial_users 
-     FROM events 
-     WHERE event_type = 'trial_started') t;
